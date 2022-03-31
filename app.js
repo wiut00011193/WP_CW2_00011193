22eb24aff8a5675dae4047d8ee33b9577f5b9ee5
wiut00011193 <wiut00011193@gmail.com>
2022-03-31 21:29:11 +0500

    nodemon installed

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.listen(port, () => {
     console.log(`Ready on port ${port}`)
 })
