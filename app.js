5afa1414de7477613a1c980dc9f4dd184b0a55e6
wiut00011193 <wiut00011193@gmail.com>
2022-04-02 12:16:06 +0500

    Navigation added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const fs = require('fs')
+
 const PORT = process.env.port || 3000
+const dbPath = path.join(__dirname, 'database/tasks.json')
 
 app.set('view engine', 'pug')
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
